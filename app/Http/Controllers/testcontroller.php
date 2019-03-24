51a3122f1523d92d2a457e1287cbef194f8a2a9e
shariarriday <shariarriday@gmail.com>
2019-03-24 22:07:44 +0600

    Updated with Insert Code

--- app/Http/Controllers/testcontroller.php
+++ app/Http/Controllers/testcontroller.php
@@ -8,7 +8,11 @@ class testcontroller extends Controller
 {
     public function test()
     {
-        $users = DB::connection('oracle')->select('select * from Provider');
-        var_dump($users);
+        $users = DB::connection('oracle')->select('select * from Provider'); //this is the prototype for select query.
+        $user = $users[0]->id; //here 0 is the index and id is the name of column in the database.
+        var_dump($user);
+        $id1 = '123';
+        $name = 'name';
+        $users = DB::connection('oracle')->insert("INSERT INTO Provider VALUES('$id1','$name')");//this is for inserting data
     }
 }
---
+++ export.sql
@@ -0,0 +1,21 @@
+--------------------------------------------------------
+--  File created - Sunday-March-24-2019   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table PROVIDER
+--------------------------------------------------------
+
+  CREATE TABLE "C##RIDAY"."PROVIDER" 
+   (	"ID" VARCHAR2(6 BYTE), 
+	"NAME" VARCHAR2(20 BYTE)
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
+REM INSERTING into C##RIDAY.PROVIDER
+SET DEFINE OFF;
+Insert into C##RIDAY.PROVIDER (ID,NAME) values ('sdf','dsfadf');
+Insert into C##RIDAY.PROVIDER (ID,NAME) values ('sdf','dsfaddf');
