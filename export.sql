e0255161de97ef8245568ff73df2ed8e136bf51f
shariarriday <shariarriday@gmail.com>
2019-03-29 23:02:59 +0600

    Update export.sql

--- export.sql
+++ export.sql
@@ -1,14 +1,14 @@
 --------------------------------------------------------
---  File created - Monday-March-25-2019   
+--  File created - Friday-March-29-2019   
 --------------------------------------------------------
-DROP TABLE "C##RIDAY"."PROVIDER" cascade constraints;
 --------------------------------------------------------
 --  DDL for Table PROVIDER
 --------------------------------------------------------
 
   CREATE TABLE "C##RIDAY"."PROVIDER" 
    (	"ID" VARCHAR2(6 BYTE), 
-	"NAME" VARCHAR2(20 BYTE)
+	"NAME" VARCHAR2(20 BYTE), 
+	"PASS" VARCHAR2(10 BYTE)
    ) SEGMENT CREATION IMMEDIATE 
   PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
  NOCOMPRESS LOGGING
@@ -18,7 +18,13 @@ DROP TABLE "C##RIDAY"."PROVIDER" cascade constraints;
   TABLESPACE "USERS" ;
 REM INSERTING into C##RIDAY.PROVIDER
 SET DEFINE OFF;
-Insert into C##RIDAY.PROVIDER (ID,NAME) values ('123','name');
-Insert into C##RIDAY.PROVIDER (ID,NAME) values ('123','name');
-Insert into C##RIDAY.PROVIDER (ID,NAME) values ('sdf','dsfadf');
-Insert into C##RIDAY.PROVIDER (ID,NAME) values ('sdf','dsfaddf');
+Insert into C##RIDAY.PROVIDER (ID,NAME,PASS) values ('adsf','dsd','dfd');
+Insert into C##RIDAY.PROVIDER (ID,NAME,PASS) values ('adsf','dsd','dfd');
+Insert into C##RIDAY.PROVIDER (ID,NAME,PASS) values ('#001','Rafa_chagol','Rafa_bolod');
+Insert into C##RIDAY.PROVIDER (ID,NAME,PASS) values ('123','name',null);
+Insert into C##RIDAY.PROVIDER (ID,NAME,PASS) values ('123','name',null);
+Insert into C##RIDAY.PROVIDER (ID,NAME,PASS) values ('adsf','ds','df');
+Insert into C##RIDAY.PROVIDER (ID,NAME,PASS) values ('adsf','ds','df');
+Insert into C##RIDAY.PROVIDER (ID,NAME,PASS) values ('adsf','ds','df');
+Insert into C##RIDAY.PROVIDER (ID,NAME,PASS) values ('sdf','dsfadf',null);
+Insert into C##RIDAY.PROVIDER (ID,NAME,PASS) values ('sdf','dsfaddf',null);
