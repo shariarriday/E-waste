b31f9bdd05d92400ed4e32a0463882ff1f06fd14
shariarriday <shariarriday@gmail.com>
2019-05-01 03:13:27 +0600

    Update

---
+++ resources/views/template/Individual.blade.php
@@ -0,0 +1,56 @@
+@include('header')
+<body style="background-image: linear-gradient(-25deg, #FFFFFF 0%, #C0C0C0 100%);">
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">E-waste</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
+
+	<div class="limiter">
+
+
+		<div class="container-table100">
+			<div class="wrap-table100">
+				<div class="table100 ver1 m-b-110">
+					<table data-vertable="ver1">
+						<thead>
+							<tr class="row100 head">
+								<th class="column100 column1" data-column="column1"></th>
+								<th class="column100 column2" data-column="column2">Sunday</th>
+								<th class="column100 column3" data-column="column3">Monday</th>
+								<th class="column100 column4" data-column="column4">Tuesday</th>
+								<th class="column100 column5" data-column="column5">Wednesday</th>
+								<th class="column100 column6" data-column="column6">Thursday</th>
+								<th class="column100 column7" data-column="column7">Friday</th>
+								<th class="column100 column8" data-column="column8">Saturday</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1">Lawrence Scott</td>
+								<td class="column100 column2" data-column="column2">{{"HELLO"}}</td>
+								<td class="column100 column3" data-column="column3">--</td>
+								<td class="column100 column4" data-column="column4">--</td>
+								<td class="column100 column5" data-column="column5">8:00 AM</td>
+								<td class="column100 column6" data-column="column6">--</td>
+								<td class="column100 column7" data-column="column7">5:00 PM</td>
+								<td class="column100 column8" data-column="column8">8:00 AM</td>
+							</tr>
+
+
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
