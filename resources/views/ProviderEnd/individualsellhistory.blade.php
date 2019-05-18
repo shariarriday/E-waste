d36667fa7113440e8ca3fe66f71a2300cc581656
nazifarafa <nhrafa.96@gmail.com>
2019-05-19 02:54:16 +0600

    buyer, provider

--- resources/views/ProviderEnd/individualsellhistory.blade.php
+++ resources/views/ProviderEnd/individualsellhistory.blade.php
@@ -1,15 +1,28 @@
 @include('header')
-<body style="background-image: linear-gradient(-25deg, #FFFFFF 0%, #C0C0C0 100%);">
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">E-waste</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li><a href="/user/Individualhome">Home</a></li>
-    </ul>
-  </div>
-</nav>
+<body style="font-family: 'Montserrat', sans-serif; background-image: linear-gradient(-25deg, #FFFFFF 0%, #C0C0C0 100%);">
+
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #2c9368; font-family: 'Montserrat', sans-serif;">
+      <a class="navbar-brand" href="#">E-waste Management</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/user/individualsellhistory">Sell History<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/user/sellItems">Sell Items <span class="sr-only">(current)</span></a>
+          </li>
+
+
+        </ul>
+      </div>
+    </nav>
 
 	<div class="limiter">
 
@@ -22,13 +35,23 @@
     ?>
 		<div class="container-table100">
 			<div class="wrap-table100">
-				<div class="table100 ver1 m-b-110">
-					<table data-vertable="ver1">
+      <!searchbarstart>  <div style="text-align: center; margin-bottom: 15px;">
+            <h3><b>Sell History</b></h3>
+        </div>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon3">Type to Filter Log</span>
+        </div>
+        <input type="text" class="form-control" id="input" aria-describedby="basic-addon3">
+      </div> <!searchbarend>
+
+				<div class="table100 ver2 m-b-110">
+					<table id =table data-vertable="ver2">
 						<thead>
 							<tr class="row100 head">
                 <th class="column100 column1" data-column="column1">Product Name</th>
 								<th class="column100 column2" data-column="column2">Sources</th>
-								<th class="column100 column3" data-column="column3">Barcode</th>
+								<th class="column100 column3" data-column="column3">Unique ID</th>
 								<th class="column100 column4" data-column="column4">Order Date</th>
 								<th class="column100 column5" data-column="column5">Product Condition</th>
 
@@ -46,7 +69,10 @@
                 $prod = DB::connection('oracle')->select("SELECT name FROM PRODUCTNAME WHERE BARCODE = '$val->barcode'");
    ?>
 							<tr class="row100">
+                @if(count($prod)!= 0)
                 <td class="column100 column1" data-column="column1">{{$prod[0]->name}}</td>
+                  @endif
+
 								<td class="column100 column2" data-column="column2">{{$val->sources}}</td>
 								<td class="column100 column3" data-column="column3">{{$val->barcode}}</td>
 								<td class="column100 column4" data-column="column4">{{$val->order_date}}</td>
@@ -62,5 +88,10 @@
 			</div>
 		</div>
 	</div>
+  <script>
+  $('#input').keyup(function () {
+    table_search($('#input').val(),$('#table tbody tr'),'0123');
+  });
+  </script>
 </body>
 </html>
